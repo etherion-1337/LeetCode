7cd723125f30dc9967961c9a0a68dd3216b68003
Zk_Xav <zhongkan.tang@gmail.com>
2024-06-17 18:29:03 +0800

    Update solution.py

--- medium/36_valid_sudoku/solution.py
+++ medium/36_valid_sudoku/solution.py
@@ -1,3 +1,6 @@
+from typing import List
+from collections import defaultdict
+
 class Solution(object):
     def isValidSudoku(self, board):
         """
@@ -31,4 +34,53 @@ class Solution(object):
             if len(filled_box) != len(set(filled_box)):
                 return False
         
+        return True
+    
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+
+        time complexity: O(9^2)
+        space complexity: O(9^2)
+        stored the filled numbers in each row, col, and box hashset
+        for boxes, we use (r//3, c//3) as the key so that we reduce from 81*81 -> 3*3 grid to identify the box
+        """
+        row_set = defaultdict(set)
+        col_set = defaultdict(set)
+        box_set = defaultdict(set)
+
+        for r in range(9): #row
+            for c in range(9): #col
+                if board[r][c] == ".":
+                    continue
+                if board[r][c] in row_set[r] or board[r][c] in col_set[c] or board[r][c] in box_set[(r//3,c//3)]:
+                    return False
+                row_set[r].add(board[r][c])
+                col_set[c].add(board[r][c])
+                box_set[(r//3,c//3)].add(board[r][c])
+    
+        return True
+    
+class NeetSolution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)  # key = (r /3, c /3)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
         return True
