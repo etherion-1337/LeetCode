b134c8cc9f1e0888822a5ebb5467e8a3e008e739
Zk_Xav <zhongkan.tang@gmail.com>
2024-06-21 14:50:47 +0800

    Update solution.py

--- medium/11_container_with_most_water/solution.py
+++ medium/11_container_with_most_water/solution.py
@@ -24,4 +24,46 @@ class Solution(object):
 
         return ans
     
+
+class Solution(object):
+    """
+    Use left and right pointers to find the maximum area.
+    Since the area is limited by the shorter height, we move the pointer to the shorter height after each iteration.
+    O(n) time complexity
+    """
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(height) - 1
+
+        ans = 0
+
+        while l != r:
+            area = min(height[l], height[r]) * (r - l)
+            ans = max(ans, area)
+            if height[l] > height[r]:
+                r -= 1
+            else:
+                l += 1
+
+        return ans
     
+class NeetSolution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(height) - 1
+        res = 0
+
+        while l < r:
+            res = max(res, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            elif height[r] <= height[l]:
+                r -= 1
+            
+        return res
