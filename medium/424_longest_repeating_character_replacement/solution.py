621262d165f27a67392b6732cdb626d44d9cf8b1
Zk_Xav <zhongkan.tang@gmail.com>
2024-06-24 16:04:35 +0800

    qn 424

---
+++ medium/424_longest_repeating_character_replacement/solution.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        n be the number of characters in the string and mmm be the number of unique characters.
+
+        Time complexity: O(nm). We iterate over each unique character once, which requires O(k) time. We move a sliding window for each unique character from left to right of the string. As the window moves, each character of the string is visited at most two times. Once when it enters the window and again when it leaves the window. This adds O(n) time complexity for each iteration. So the final time complexity is O(nm). For all uppercase English letters, the maximum value of m would be 26.
+
+        Space complexity: O(m). We use an auxiliary set to store all unique characters, so the space complexity required here is O(m). Since there are only uppercase English letters in the string, m=26
+
+        1) find all unique letters in the string
+        2) sliding window for each unique letters: 
+        2.1) count the number of the letter in the window
+        2.2) if the window is valid (r - l + 1) - count <= k, then keep expanding the window and increase count if any of the current letter is added
+        2.3) if the window is invalid, then shrink the window from the left side until it is valid and decrease count if any of the current letter is removed
+        """
+        unique_letters = set(s)
+        max_length = 0
+
+        for letter in unique_letters:
+            l = 0
+            count = 0
+
+            for r in range(len(s)):
+                if s[r] == letter:
+                    count += 1
+
+                while not (r - l + 1) - count <= k:
+                    if s[l] == letter:
+                        count -= 1
+                    l += 1
+            
+                max_length = max(max_length, r - l + 1)
+        
+        return max_length
