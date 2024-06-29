f8d5cc964ca5282add12afa6564b67cf89073e50
Zk_Xav <zhongkan.tang@gmail.com>
2024-06-29 21:21:35 +0800

    Update solution.py

--- medium/150_evaluate_reverse_polish_notation/solution.py
+++ medium/150_evaluate_reverse_polish_notation/solution.py
@@ -5,6 +5,7 @@ class Solution:
         """
         Use stack to store the numbers and pop the top 2 numbers to calculate the result
         insert the result back to the top of the stack
+        time complexity: O(n)
         """
         stack = []
         sign = ["+", "-", "/", "*"]
@@ -25,4 +26,22 @@ class Solution:
                 stack.pop()
                 stack.append(_top)
         return int(stack[0])
+    
+class NeetSolution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b) / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
     
