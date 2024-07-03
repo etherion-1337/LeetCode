b7221fedf152cb5201aae1b7057c57ca3c2d2a58
Zk_Xav <zhongkan.tang@gmail.com>
2024-07-03 21:53:52 +0800

    Update solution.py

--- medium/875_koko_eating_bananas/solution.py
+++ medium/875_koko_eating_bananas/solution.py
@@ -1,9 +1,11 @@
 from typing import List
+import math
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         """
         Brute force solution
+        time complexity: O(max(piles) * n)
         """
         for i in range(max(piles), 0, -1):
             _list = [-(-pile//i) for pile in piles]
@@ -12,5 +14,30 @@ class Solution:
                 return i + 1
 
         return max(piles)
+    
+class NeetSolution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        """
+        same idea with brute force solution but with binary search
+        search from 1 to max(piles) since max(piles) is the upper bound of the answer: eat maximum pile in 1 hour
+        time complexity: O(nlog(max(piles))*n)
+        """
+        l = 1
+        r = max(piles)
+        ans = r
+
+        while l <= r:
+            k = (l+r)//2
+
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p/k)
+            if hours <= h:
+                ans = k
+                r = k - 1
+            else:
+                l = k + 1
+
+        return ans
     
 
