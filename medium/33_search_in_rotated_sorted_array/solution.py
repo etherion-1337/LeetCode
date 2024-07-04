9a5bc4b0125d932875fa4476aa0017d573878431
Zk_Xav <zhongkan.tang@gmail.com>
2024-07-04 22:25:14 +0800

    Update solution.py

--- medium/33_search_in_rotated_sorted_array/solution.py
+++ medium/33_search_in_rotated_sorted_array/solution.py
@@ -36,3 +36,25 @@ class Solution:
                         l = mid + 1 # target remained at right side
 
         return -1
+    
+class NeetSolution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+                    
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
