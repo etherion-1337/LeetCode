fd46e442c0bb075941275e00b03d227de98d58a9
Zk_Xav <zhongkan.tang@gmail.com>
2024-07-30 18:41:36 +0800

    Update solution.py

--- easy/206_reverse_linked_list/solution.py
+++ easy/206_reverse_linked_list/solution.py
@@ -57,3 +57,13 @@ class Solution:
         return newHead
     
 
+if __name__ == '__main__':
+    # test case
+    # head = ListNode(1)
+    # head.next = ListNode(2)
+    # head.next.next = ListNode(3)
+    # head.next.next.next = ListNode(4)
+    # head.next.next.next.next = ListNode(5)
+    l1 = ListNode(1,ListNode(2, ListNode(3, None)))
+    s = Solution()
+    print(s.reverseList(l1))  # expect 5 -> 4 -> 3 -> 2 -> 1 -> None
