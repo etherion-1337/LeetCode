e873226b2f9ee45f9bc7a33897435053613a8b86
Zk_Xav <zhongkan.tang@gmail.com>
2024-08-03 21:03:08 +0800

    Update solution.py

--- medium/138_copy_list_with_random_pointer/solution.py
+++ medium/138_copy_list_with_random_pointer/solution.py
@@ -11,3 +11,31 @@ class CheeseSolution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         new_copy = copy.deepcopy(head)
         return new_copy
+    
+class Solution:
+    """
+    Two pass solution
+    time: O(n)
+    space: O(n)
+    """
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # hash map to store mapping from old node to new node (val only)
+        # None is for the edge case where the random is point to None
+        old2new_map = {None:None}
+
+        # go through the list to copy val only
+        curr = head
+        while curr:
+            old2new_map[curr] = Node(curr.val)
+            curr = curr.next
+
+        # go through the list again to link
+        curr = head
+        while curr:
+            copy = old2new_map[curr]
+            # now fill next and random
+            copy.next = old2new_map[curr.next]
+            copy.random = old2new_map[curr.random]
+            curr = curr.next
+
+        return old2new_map[head]
