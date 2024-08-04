b7791fdcc5977c5e111ef614d3dd5cc64ce8fa11
Zk_Xav <zhongkan.tang@gmail.com>
2024-08-04 16:36:17 +0800

    Update solution.py

--- easy/141_linked_list_cycle/solution.py
+++ easy/141_linked_list_cycle/solution.py
@@ -10,6 +10,8 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         """
         use a hash set to store the visited nodes
+        time complexity: O(n)
+        space complexity: O(n)
         """
         hash_set = []
         curr = head
@@ -21,4 +23,27 @@ class Solution:
                 return True
             curr = curr.next
             
+        return False
+    
+class NeetSolution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        """
+        space O(1) solution
+        use two pointers, slow and fast
+        fast to catch up slow, if there is a cycle, they will meet
+        every iteration, the distance between slow and fast will decreased by 1 if there is a cycle
+        therefore the time complexity is O(n)
+        time complexity: O(n)
+        space complexity: O(1)
+        """
+        slow, fast = head, head
+
+        # fast need to jump twice, so need to make sure is not None 
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
         return False
