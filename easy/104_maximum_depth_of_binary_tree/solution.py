470680a33a8fb4ce20612536386cc25b50a7a1aa
Zk_Xav <zhongkan.tang@gmail.com>
2024-08-08 15:15:07 +0800

    Update solution.py

--- easy/104_maximum_depth_of_binary_tree/solution.py
+++ easy/104_maximum_depth_of_binary_tree/solution.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+from collections import deque
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -7,9 +7,11 @@ class TreeNode:
         self.left = left
         self.right = right
 
-class Solution:
+class DFSSolutionRecursive:
     """
     Recursive DFS solution
+    time complexity: O(n)
+    space complexity: O(n)
     """
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
@@ -19,4 +21,49 @@ class Solution:
         rightmax = self.maxDepth(root.right)
 
         return 1 + max(leftmax, rightmax)
+
+class BFSSolution:
+    """
+    iterative BFS
+    use a deque to store the nodes in the same level
+    time complexity: O(n)
+    space complexity: O(n)
+    """
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        level = 0
+        q = deque([root])
+
+        while q:
+            # take a snap shot at current q
+            # replace all nodes with their children
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            level += 1
+
+        return level
     
+class DFSSolutionIterative:
+    """
+    Iterative DFS solution
+    use a stack to store the nodes and their depth
+    """
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
