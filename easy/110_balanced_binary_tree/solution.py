c33d0bc33e5951994c448fe225cfc16d18720676
Zk_Xav <zhongkan.tang@gmail.com>
2024-08-09 21:49:17 +0800

    Update solution.py

--- easy/110_balanced_binary_tree/solution.py
+++ easy/110_balanced_binary_tree/solution.py
@@ -35,3 +35,19 @@ class Solution:
         height = self.dfs(root)
         
         return self.balanced
+    
+class NeetSolution:
+    """
+    Almost identical to the above solution, but with a different way of returning the height of the subtree.
+    """
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root:
+                return [True, 0]
+
+            left, right = dfs(root.left), dfs(root.right)
+            # make sure not only the current node is balanced, but also the left and right subtree are balanced
+            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+            return [balanced, 1 + max(left[1], right[1])]
+
+        return dfs(root)[0]
