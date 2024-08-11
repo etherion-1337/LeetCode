4167df5f5826d89b9768824c5532592d474c4487
Zk_Xav <zhongkan.tang@gmail.com>
2024-08-11 17:44:53 +0800

    Update solution.py

--- easy/572_subtree_of_another_tree/solution.py
+++ easy/572_subtree_of_another_tree/solution.py
@@ -29,6 +29,9 @@ class Solution:
         same as Qn 100: Same Tree
         """
         if not root and not subroot:
+        # this covers the edge case where root is not None but subroot is None -> yes it is subtree
+        # think of it the leaf node will lead to None child node which is the same as the (Null) subroot
+        # this also covers the edge case where both root and subroot are None -> yes it is subtree
             return True
         elif not root or not subroot:
             return False
@@ -39,3 +42,30 @@ class Solution:
         right_check = self.isSameTree(root.right, subroot.right)
 
         return (left_check and right_check)
+    
+class NeetSolution:
+    """
+    time complexity: O(n * m) where n is the number of nodes in the main tree and m is the number of nodes in the subtree
+    """
+    
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot:         # i think this first if statement is not necessary, this will be covered by the first if in the sameTree function, when it traverse to the end of the main tree
+            return True
+        # order matters, here is really "if not root and subRoot", but subRoot is checked in the first if statement
+        # this if statement is necessary for isSubtree to backtrack when it reaches the end of main tree
+        # it also covers the edge case where root is None but subRoot is not None -> no, not a subtree
+        # although the this case is also covered in the (return) exit condition of the sameTree function
+        if not root:
+            return False
+
+        if self.sameTree(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def sameTree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root and not subRoot:
+            return True
+        if root and subRoot and root.val == subRoot.val:
+            return self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right)
+        # this means that one of the root/subRoot is None and the other is not, hence not the same tree
+        return False
