9e4584f50d7153bbc367603d621037f16a9a6ea8
Zk_Xav <zhongkan.tang@gmail.com>
2024-08-17 16:57:44 +0800

    qn 297

---
+++ hard/297_serialize_and_deserialize_binary_tree/solution.py
@@ -0,0 +1,62 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Codec:
+    """
+    Since the all leaf node is specified as "N", this can be considered as a full binary tree.
+    So we can only use the preorder traversal to serialize and deserialize the tree.
+    If not we need use inorder too.
+    """
+    def preorder_dfs_ser(self, curr):
+        if not curr:
+            self.preorder_arr.append("N")
+            return 
+
+        self.preorder_arr.append(str(curr.val))
+        self.preorder_dfs_ser(curr.left)
+        self.preorder_dfs_ser(curr.right)
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        self.preorder_arr = []
+        self.preorder_dfs_ser(root)
+    
+        data = ",".join(self.preorder_arr)
+        return data
+
+    def preorder_dfs_deser(self, data_list):
+        if data_list[self.pointer] == "N":
+            self.pointer += 1
+            return None
+        
+        node = TreeNode(int(data_list[self.pointer]))
+        self.pointer += 1
+        node.left = self.preorder_dfs_deser(data_list)
+        node.right = self.preorder_dfs_deser(data_list)
+        return node
+
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        data_list = data.split(",")
+        self.pointer = 0
+
+        root = self.preorder_dfs_deser(data_list)
+        return root
+        
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
