c81f7cacbaef585b2d41be1ba4ee61c57a86fea3
Zk_Xav <zhongkan.tang@gmail.com>
2024-08-19 12:47:12 +0800

    Update solution.py

--- easy/1046_last_stone_weight/solution.py
+++ easy/1046_last_stone_weight/solution.py
@@ -2,6 +2,9 @@ import heapq
 from typing import List
 
 class Solution:
+    """
+    heap pop and push are O(logN) operations because of the heap property
+    """
     def lastStoneWeight(self, stones: List[int]) -> int:
         self.minHeap = [-abs(stone) for stone in stones]
         heapq.heapify(self.minHeap)
@@ -19,3 +22,17 @@ class Solution:
                 heapq.heappush(self.minHeap, x - y)
 
         return -self.minHeap[0] if self.minHeap else 0
+    
+class NeetSolution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            first = heapq.heappop(stones)
+            second = heapq.heappop(stones)
+            if second > first:
+                heapq.heappush(stones, first - second)
+        # for edge case where no stone is left after the loop
+        stones.append(0)
+        return abs(stones[0])
