dc4935cdb5cb3f5ec7fb7d5a6b34280a79b2bb56
Zk_Xav <zhongkan.tang@gmail.com>
2024-08-20 10:23:14 +0800

    Update solution.py

--- medium/973_k_closest_points_to_origin/solution.py
+++ medium/973_k_closest_points_to_origin/solution.py
@@ -5,6 +5,9 @@ from typing import List
 class Solution:
     """
     min heap is used to store the distance of each point to the origin
+    heapify is O(n) operation
+    popping k element is O(klogn) operation
+    if we do sorting it will be O(nlogn) operation
     """
     def calc_dist2orig(self, point):
         return math.sqrt((point[0]-0)**2 + (point[1]-0)**2)
@@ -15,3 +18,18 @@ class Solution:
         ans_raw = heapq.nsmallest(k, minHeap)
         ans = [res[1] for res in ans_raw]
         return ans
+    
+class NeetSolution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        minHeap = []
+        for x, y in points:
+            # if just comparison is needed, we can avoid using sqrt
+            dist = (x ** 2) + (y ** 2)
+            minHeap.append((dist, x, y))
+        
+        heapq.heapify(minHeap)
+        res = []
+        for _ in range(k):
+            _, x, y = heapq.heappop(minHeap)
+            res.append((x, y))
+        return res
