674789b6808fe10f4ff1c26ff4694589a7054f2b
Zk_Xav <zhongkan.tang@gmail.com>
2024-08-26 18:16:23 +0800

    Update solution.py

--- medium/46_permutations/solution.py
+++ medium/46_permutations/solution.py
@@ -29,3 +29,48 @@ class Solution:
         self.dfs()
 
         return self.result
+    
+
+class NeetSolution:
+    """
+    Recursive solution
+    1. Base case: if nums is empty, return [[]]
+    2. Get all permutations of nums[1:]
+    3. For each permutation, insert nums[0] at every possible position
+    4. Append the new permutation to the result
+
+    Time complexity: O(n! * n^2)
+    space complexity: O(n! * n)
+    """
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        if len(nums) == 0:
+            return [[]]
+
+        perms = self.permute(nums[1:])
+        res = []
+
+        for p in perms:
+            # since we can insert at len(p) + 1 positions
+            for i in range(len(p) + 1):
+                p_copy = p.copy()
+                p_copy.insert(i, nums[0])
+                res.append(p_copy)
+    
+        return res
+    
+class NeetSolution_2:
+    """
+    Iterative solution
+    """
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        perms = [[]]
+        for n in nums:
+            new_perms = []
+            for p in perms:
+                for i in range(len(p) + 1):
+                    p_copy = p.copy()
+                    p_copy.insert(i, n)
+                    new_perms.append(p_copy)
+            perms = new_perms
+        return perms
