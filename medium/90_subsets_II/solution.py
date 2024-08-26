33421dbb710442388c240660b06b24dd99c6a66d
Zk_Xav <zhongkan.tang@gmail.com>
2024-08-26 23:59:37 +0800

    Update solution.py

--- medium/90_subsets_II/solution.py
+++ medium/90_subsets_II/solution.py
@@ -22,3 +22,28 @@ class Solution:
 
         self.dfs(0,nums)
         return self.result
+    
+
+class NeetSolution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        # sort so that duplicates are adjacent
+        nums.sort()
+
+        def backtrack(i, subset):
+            if i == len(nums):
+                res.append(subset[::])
+                return
+
+            # all subsets that include nums[i]
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+            subset.pop()
+            # all subsets that exclude nums[i]
+            # move pointer to next different number
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            backtrack(i + 1, subset) # even if we run through the while loop, this will gives [] which is still valid
+
+        backtrack(0, [])
+        return res
