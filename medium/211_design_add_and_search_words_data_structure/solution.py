580579843e7e3a660f610573e4bccaa8a9c4ea77
Zk_Xav <zhongkan.tang@gmail.com>
2024-09-02 17:05:25 +0800

    Update solution.py

--- medium/211_design_add_and_search_words_data_structure/solution.py
+++ medium/211_design_add_and_search_words_data_structure/solution.py
@@ -24,14 +24,14 @@ class WordDictionary:
             char = word[i]
             if char == ".":
                 for child in curr.children.values():
-                    if self.dfs(i+1, child, word):
+                    if self.dfs(i+1, child, word): # i + 1 because we are skipping "."
                         return True
                 return False
             else:
                 if char not in curr.children:
                     return False
                 curr = curr.children[char]
-        return curr.end_of_word
+        return curr.end_of_word # e.g. if a full word is found, then only else is executed and return True
         
     def search(self, word: str) -> bool:
         search_result = self.dfs(0, self.root, word)
@@ -44,3 +44,41 @@ class WordDictionary:
 # obj = WordDictionary()
 # obj.addWord(word)
 # param_2 = obj.search(word)
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}  # a : TrieNode
+        self.word = False
+
+
+class NeetWordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.word = True
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            cur = root
+
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.word
+
+        return dfs(0, self.root)
