7880e52226e525a7d68d9adc8371cfd19e5002e8
Zk_Xav <zhongkan.tang@gmail.com>
2024-09-04 09:41:49 +0800

    Update solution.py

--- medium/695_max_area_of_island/solution.py
+++ medium/695_max_area_of_island/solution.py
@@ -43,3 +43,66 @@ class Solution:
                     self.result_arr.append(area)
 
         return max(self.result_arr) if self.result_arr else 0
+    
+class Solution:
+    """
+    DFS solution (recursive)
+    time and space complexity: O(m*n)
+    """
+    def dfs(self, r, c):
+        if (
+            r not in range(self.rows)
+            or c not in range(self.cols)
+            or self.grid[r][c] == 0
+            or (r, c) in self.visit
+        ):
+            return
+
+        self.area += 1
+        self.visit.add((r, c))
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        for dr, dc in directions:
+            self.dfs(r + dr, c + dc)
+
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        self.grid = grid
+
+        if not self.grid or not self.grid[0]:
+            return 0
+
+        area_list = []
+        self.visit = set()
+        self.rows, self.cols = len(grid), len(grid[0])
+
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if self.grid[r][c] == 1 and (r, c) not in self.visit:
+                    self.area = 0
+                    self.dfs(r, c)
+                    area_list.append(self.area)
+
+        return max(area_list) if area_list else 0
+    
+class NeetSolution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+
+        def dfs(r, c):
+            if (
+                r < 0
+                or r == ROWS
+                or c < 0
+                or c == COLS
+                or grid[r][c] == 0
+                or (r, c) in visit
+            ):
+                return 0
+            visit.add((r, c))
+            return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
+
+        area = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                area = max(area, dfs(r, c))
+        return area
