56365650d9c10951450edd3f432350ddb8cc10e8
Zk_Xav <zhongkan.tang@gmail.com>
2024-10-29 23:25:24 +0800

    Update solution.py

--- easy/118_pascal_triangle/solution.py
+++ easy/118_pascal_triangle/solution.py
@@ -31,3 +31,25 @@ class NeetSolution:
             res.append(row)
         
         return res
+
+
+class NeetSolution:
+    """
+    recursive solution
+    """
+    def generate(self, rowIndex) -> List[List[int]]:
+        if rowIndex == 0:
+            return [[1]]
+        else:
+            return self.getAllRow(rowIndex - 1)
+
+    def getAllRow(self, rowIndex):
+        if rowIndex == 0:
+            return [[1]]
+        ListPrec = self.getAllRow(rowIndex - 1)
+        Len = len(ListPrec[-1])
+        ListPrec.append([1])
+        for i in range(0, Len - 1):
+            ListPrec[-1].append(ListPrec[-2][i] + ListPrec[-2][i + 1])
+        ListPrec[-1].append(1)
+        return ListPrec
