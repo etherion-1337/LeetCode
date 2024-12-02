b5036f9d1e3c5208cc8a85bc86a51d6bd3a2dc65
Zk_Xav <zhongkan.tang@gmail.com>
2024-12-02 23:15:38 +0800

    1455

---
+++ easy/1455_check_if_a_word_occurs_as_a_prefix_of_any_word_in_a_sentence/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        word_list = sentence.split()
+
+        for i in range(len(word_list)):
+            if word_list[i].startswith(searchWord):
+                return i + 1
+
+        else:
+            return -1
