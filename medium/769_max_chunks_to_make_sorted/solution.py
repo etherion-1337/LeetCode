32eb1a8dfe2fb4e9777bfd12c05a3684be0537eb
Zk_Xav <zhongkan.tang@gmail.com>
2024-12-19 22:55:07 +0800

    Update solution.py

--- medium/769_max_chunks_to_make_sorted/solution.py
+++ medium/769_max_chunks_to_make_sorted/solution.py
@@ -1,10 +1,15 @@
 from typing import List
 
 class Solution:
+    """
+    greedy approach
+    """
     def maxChunksToSorted(self, arr: List[int]) -> int:
         curr_max = -1
         res = 0
-
+        # the max value has to appear at the index of the max value for the chunk to be sorted
+        # since there is no duplicate, if the max value is at the index of the max value, then all the values before it are smaller
+        # if it were to have bigger values before it, then the max value would have been bigger
         for i, n in enumerate(arr):
             curr_max = max(n, curr_max)
 
