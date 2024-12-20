a78f801ed80a8d1e370c6bb730b69e997750d3d9
Zk_Xav <zhongkan.tang@gmail.com>
2024-12-20 10:59:47 +0800

    2415

---
+++ medium/2415_reverse_odd_levels_of_binary_tree/solution.py
@@ -0,0 +1,36 @@
+from collections import deque
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    """
+    BFS solution
+    """
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        q = deque([root])
+        level = 0
+
+        while q:
+            if level % 2:
+                l, r = 0, len(q) - 1
+                while l <= r:
+                    q[l].val, q[r].val = q[r].val, q[l].val
+                    l += 1
+                    r -= 1
+
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            level += 1
+
+        return root
