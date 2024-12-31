43069963647e4e022d65bebc86c09cbe86ce8103
Zk_Xav <zhongkan.tang@gmail.com>
2024-12-31 13:08:09 +0800

    983

---
+++ medium/983_minimum_cost_for_tickets/solution.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        dp = {}
+
+        def dfs(i):
+            if i == len(days):
+                return 0
+            if i in dp:
+                return dp[i]
+
+            ans = float("inf")
+            j = i
+            for cost, duration in zip(costs, [1, 7, 30]):
+                while j < len(days) and days[j] < days[i] + duration:
+                    j += 1
+                ans = min(ans, cost + dfs(j))
+            dp[i] = ans
+            return ans
+
+        return dfs(0)
