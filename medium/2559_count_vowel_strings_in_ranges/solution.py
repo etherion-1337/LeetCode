f7a25017d35b980c835c0fc71051c371df2f8746
Zk_Xav <zhongkan.tang@gmail.com>
2025-01-02 22:08:36 +0800

    2559

---
+++ medium/2559_count_vowel_strings_in_ranges/solution.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    """
+    prefix sum
+
+    time complexity: O(n)
+    space complexity: O(n)
+    """
+    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        def check_valid(word):
+            if word[0] in ["a", "e", "i", "o", "u"] and word[-1] in ["a", "e", "i", "o", "u"]:
+                return True
+
+        tmp = [1 if check_valid(word) else 0 for word in words]
+
+        prefix_sum = []
+        curr_sum = 0
+        for _tmp in tmp:
+            curr_sum += _tmp
+            prefix_sum.append(curr_sum)
+        prefix_sum = [0] + prefix_sum + [prefix_sum[-1]]
+        ans = []
+
+        for start, end in queries:
+            ans.append(prefix_sum[end + 1] - prefix_sum[start])
+
+        return ans
