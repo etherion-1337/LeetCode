f40840241456e2237e0aab0dd03c87124adbea08
Zk_Xav <zhongkan.tang@gmail.com>
2025-01-02 22:34:43 +0800

    Update solution.py

--- medium/2559_count_vowel_strings_in_ranges/solution.py
+++ medium/2559_count_vowel_strings_in_ranges/solution.py
@@ -26,3 +26,22 @@ class Solution:
             ans.append(prefix_sum[end + 1] - prefix_sum[start])
 
         return ans
+    
+
+class NeetSolution:
+    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        vowel_set = set("aeiou")
+
+        prefix_cnt = [0] * (len(words) + 1)
+        prev = 0
+
+        for i, w in enumerate(words):
+            if w[0] in vowel_set and w[-1] in vowel_set:
+                prev += 1
+            prefix_cnt[i + 1] = prev
+
+        ans = [0] * len(queries)
+        for i, q in enumerate(queries):
+            l, r = q
+            ans[i] = prefix_cnt[r + 1] - prefix_cnt[l]
+        return ans
