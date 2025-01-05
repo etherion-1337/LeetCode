3573ed1bdb5e7a3b0b9f7428cbd736e0e672f9e8
Zk_Xav <zhongkan.tang@gmail.com>
2025-01-05 23:03:44 +0800

    Update solution.py

--- easy/169_majority_element/solution.py
+++ easy/169_majority_element/solution.py
@@ -9,3 +9,30 @@ class Solution:
             count[n] += 1
 
         return max(count, key = count.get)
+    
+class NeetSolution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = {}
+        ans, maxCount = 0, 0
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+            ans = n if count[n] > maxCount else ans
+            maxCount = max(count[n], maxCount)
+        return ans
+    
+class NeetSolution:
+    """
+    Boyer-Moore majority vote algorithm
+
+    Time complexity: O(n)
+    space complexity: O(1)
+    """
+    def majorityElement(self, nums: List[int]) -> int:
+        ans, count = 0, 0
+
+        for n in nums:
+            if count == 0:
+                ans = n
+            count += (1 if n == ans else -1)
+        return ans
