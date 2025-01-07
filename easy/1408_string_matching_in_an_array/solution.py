206dbe30bd1a0dc9b9028241df3f6f0ff178a483
Zk_Xav <zhongkan.tang@gmail.com>
2025-01-07 10:05:39 +0800

    1408

---
+++ easy/1408_string_matching_in_an_array/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    """
+    brute force solution
+
+    time complexity: O(n^2)
+    space complexity: O(n)
+    """
+    def stringMatching(self, words: List[str]) -> List[str]:
+        ans = set()
+        for w_1 in words:
+            for w_2 in words:
+                if w_1 in w_2 and w_1 != w_2:
+                    ans.add(w_1)
+
+        return list(ans)
