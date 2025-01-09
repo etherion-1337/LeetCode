d83565e369d3b9f3e0f1207ad9cf817c6beb43aa
Zk_Xav <zhongkan.tang@gmail.com>
2025-01-09 11:57:55 +0800

    Update solution.py

--- easy/2185_counting_words_with_a_given_prefix/solution.py
+++ easy/2185_counting_words_with_a_given_prefix/solution.py
@@ -8,3 +8,41 @@ class Solution:
                 ans += 1
 
         return ans
+    
+
+class TrieNode():
+    """
+    Trie solution
+    """
+    def __init__(self):
+        self.children = {} # a -> TrieNode
+        self.count = 0
+
+class Trie():
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add(self, w):
+        curr = self.root
+        for c in w:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+            curr.count += 1
+
+    def count(self, pref):
+        curr = self.root
+        for c in pref:
+            if c not in curr.children:
+                return 0
+            curr = curr.children[c]
+
+        return curr.count
+
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        prefix_tree = Trie()
+
+        for w in words:
+            if len(w) >= len(pref):
+                prefix_tree.add(w)
