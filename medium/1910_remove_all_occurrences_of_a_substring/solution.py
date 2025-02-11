3da0e2569d2f7bfff862772952562051772d928a
Zk_Xav <zhongkan.tang@gmail.com>
2025-02-11 11:38:32 +0800

    1910

---
+++ medium/1910_remove_all_occurrences_of_a_substring/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        stack = []
+        pop_length = len(part) - 1 
+
+        for c in s:
+            if len(part) == 1: # edge cases
+                if c == part:
+                    continue
+            tmp = stack[-pop_length:]
+            tmp.append(c)
+            tmp_str = "".join(tmp)
+            if tmp_str == part:
+                count = pop_length
+                while count > 0:
+                    stack.pop()
+                    count -= 1
+            else:
+                stack.append(c)
+
+        return "".join(stack)
