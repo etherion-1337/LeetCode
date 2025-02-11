d9888e4cbbe24fb8a7b4e0c9b7e9f01dee7bca2e
Zk_Xav <zhongkan.tang@gmail.com>
2025-02-11 17:26:00 +0800

    Update solution.py

--- medium/1910_remove_all_occurrences_of_a_substring/solution.py
+++ medium/1910_remove_all_occurrences_of_a_substring/solution.py
@@ -19,3 +19,19 @@ class Solution:
                 stack.append(c)
 
         return "".join(stack)
+    
+
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        stack = []
+        M = len(part)
+
+        for c in s:
+            stack.append(c)
+
+            if len(stack) >= M:
+                if "".join(stack[-M:]) == part:
+                    for _ in range(M):
+                        stack.pop()
+
+        return "".join(stack)
