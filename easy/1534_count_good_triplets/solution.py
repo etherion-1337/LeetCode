08afea64715f7c639fb1dbbee914336d5aeb86dc
Zk_Xav <zhongkan.tang@gmail.com>
2025-04-14 13:43:42 +0800

    1534

---
+++ easy/1534_count_good_triplets/solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    """
+    Brute force solution
+    """
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        ans = 0
+        for i in range(0, len(arr)):
+            for j in range(i + 1, len(arr)):
+                for k in range(j + 1, len(arr)):
+                    if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                        trip = (arr[i], arr[j], arr[k])
+                        ans += 1
+
+        return ans
